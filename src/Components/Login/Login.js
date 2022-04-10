9db46fc26ba2a07e6f5fd3a4486f04ca463a6667
abdulawalemon1 <emondx4@gmail.com>
2022-04-11 02:39:10 +0600

    google provider added

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const { signInWithGoogle } = useFirebase();
     return (
         <div>
             <h2>Login</h2>
+            <div>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
             <form >
+
+                <br />
                 <input type="email" placeholder='Your Email' />
                 <br />
                 <input type="password" name="" id="" placeholder='Your Password' />
